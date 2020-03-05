da60919b2fd28355e9e402aa77e37d35e50ecc64
Martin Karp <martinkarp@n138-p179.eduroam.kth.se>
2020-03-05 15:36:08 +0100

    2d-tensor version

--- src/ax_cuda.f
+++ src/ax_cuda.f
@@ -1,6 +1,5 @@
 #ifdef _CUDA
-
-      attributes(global) subroutine ax_cuf2(w,u,ur,us,ut,
+           attributes(global) subroutine ax_cuf2_org(w,u,ur,us,ut,
      &                gxyz,dxm1,dxtm1)
 
       include 'SIZE'
@@ -18,7 +17,6 @@
 
       real rtmp,stmp,ttmp,wijke
       real, shared :: shdxm1(lx1,ly1)
-      real, shared :: shdxtm1(lx1,ly1)
       integer l,e,i,j,k,kk,n,nstrides
 
       e = blockIdx%x
@@ -26,23 +24,10 @@
       j = threadIdx%y
       i = threadIdx%x
 
-      if (k.eq.1) then
-         shdxm1(i,j) = dxm1(i,j)
-         shdxtm1(i,j) = dxtm1(i,j)
-      end if
+      shdxm1(i,j) = dxm1(i,j)
       call syncthreads()
 
-c Figure out how many strided accesses that this block needs to perform
-      nstrides = lz1 / blockDim%z
-      if (mod(lz1, blockDim%z) .gt. 0) then
-        nstrides = nstrides + 1
-      endif
-
-c Perform the strided accesses.  Each thread in the block proceeds in
-c lockstep.
-      kk = k
-      do n = 1, nstrides
-        if (kk .le. lz1) then
+      do kk = 1, lz1
           rtmp = 0.0
           stmp = 0.0
           ttmp = 0.0
@@ -60,36 +45,181 @@ c lockstep.
           ut(i,j,kk,e) = gxyz(i,j,kk,3,e)*rtmp
      $                 + gxyz(i,j,kk,5,e)*stmp
      $                 + gxyz(i,j,kk,6,e)*ttmp
-        endif
-        kk = kk + blockDim%z
-c       rahaman 2017-03-31: The optimized kernels (e.g.nek_kernel16.cuf)
-c       called synchthreads after each strided access.  I don't believe
-c       this is necessary.  When I omit the thread sync, I see no
-c       runtime erros and the solution matches CPU version
-c       call syncthreads()
       enddo
 
       call syncthreads()
 
-      kk = k
-      do n = 1, nstrides
-        if (kk .le. lz1) then
+      do kk = 1, lz1
           wijke = 0.0
           do l = 1, lx1
-            wijke = wijke + shdxtm1(i,l)  * ur(l,j,kk,e) 
-     $                    + shdxtm1(j,l)  * us(i,l,kk,e)
-     $                    + shdxtm1(kk,l) * ut(i,j,l,e)
+            wijke = wijke + shdxm1(l,i)  * ur(l,j,kk,e) 
+     $                    + shdxm1(l,j)  * us(i,l,kk,e)
+     $                    + shdxm1(l,kk) * ut(i,j,l,e)
           enddo
           w(i,j,kk,e) = wijke
-        endif
-        kk = kk + blockDim%z
-c       rahaman 2017-03-31: The optimized kernels (e.g.nek_kernel16.cuf)
-c       called synchthreads after each strided access.  I don't believe
-c       this is necessary.  When I omit the thread sync, I see no
-c       runtime erros and the solution matches CPU version
-c       call syncthreads()
       enddo
 
+      return
+      end
+      attributes(global) subroutine ax_cuf2_naive(w,u,ur,us,ut,
+     &                gxyz,dxm1,dxtm1)
+
+      include 'SIZE'
+
+      real, intent(out) :: w(lx1,ly1,lz1,lelt)
+      real, intent(in) :: u(lx1,ly1,lz1,lelt)
+      real ur  (lx1,ly1,lz1,lelt)
+      real us  (lx1,ly1,lz1,lelt)
+      real ut  (lx1,ly1,lz1,lelt)
+
+      real, intent(in) :: gxyz(lx1,ly1,lz1,2*ldim,lelt)
+
+      real, intent(in) :: dxm1(lx1,lx1)
+      real, intent(in) :: dxtm1(lx1,lx1)
+
+      real rtmp,stmp,ttmp,wijke
+      real, shared :: shdxm1(lx1,ly1)
+      real, shared :: shus(lx1,ly1,lz1)
+      real, shared :: shur(lx1,ly1,lz1)
+      real t_kk(lz1)
+      real shut(lz1)
+      integer l,e,i,j,k,kk,n
+
+      e = blockIdx%x
+      k = threadIdx%z
+      j = threadIdx%y
+      i = threadIdx%x
+      do kk = 1, lz1
+         w(i,j,kk,e) = 0. 
+      enddo
+      shdxm1(i,j) = dxm1(i,j)
+      call syncthreads()
+
+      do kk = 1,lz1
+          ttmp = 0.0
+          do l = 1, lx1
+            ttmp = ttmp + shdxm1(kk,l) * u(i,j,l,e)
+          enddo
+          t_kk(kk) = ttmp 
+      enddo
+
+      call syncthreads()
+      do kk = 1,lz1
+          rtmp = 0.0
+          stmp = 0.0
+          do l = 1, lx1
+            rtmp = rtmp + shdxm1(i,l)  * u(l,j,kk,e)
+            stmp = stmp + shdxm1(j,l)  * u(i,l,kk,e)
+          enddo
+          shur(i,j,kk) = gxyz(i,j,kk,1,e)*rtmp
+     $                 + gxyz(i,j,kk,2,e)*stmp
+     $                 + gxyz(i,j,kk,3,e)*t_kk(kk)
+          shus(i,j,kk) = gxyz(i,j,kk,2,e)*rtmp
+     $                 + gxyz(i,j,kk,4,e)*stmp
+     $                 + gxyz(i,j,kk,5,e)*t_kk(kk)
+          shut(kk) =     gxyz(i,j,kk,3,e)*rtmp
+     $                 + gxyz(i,j,kk,5,e)*stmp
+     $                + gxyz(i,j,kk,6,e)*t_kk(kk)
+          
+          do l = 1, lx1
+             w(i,j,l,e) = w(i,j,l,e) + shdxm1(kk,l)*shut(kk)
+          enddo
+      enddo
+      call syncthreads()
+      do kk = 1, lz1 
+          wijke = 0.0
+          do l = 1, lx1
+            wijke = wijke + shdxm1(l,i)  * shur(l,j,kk) 
+     $                    + shdxm1(l,j)  * shus(i,l,kk)
+          enddo
+          w(i,j,kk,e) = w(i,j,kk,e) + wijke
+      enddo
+      return
+      end
+
+
+      attributes(global) subroutine ax_cuf2(w,u,ur,us,ut,
+     &                gxyz,dxm1,dxtm1)
+
+      include 'SIZE'
+
+      real, intent(out) :: w(lx1,ly1,lz1,lelt)
+      real, intent(in) :: u(lx1,ly1,lz1,lelt)
+      real ur  (lx1,ly1,lz1,lelt)
+      real us  (lx1,ly1,lz1,lelt)
+      real ut  (lx1,ly1,lz1,lelt)
+
+      real, intent(in) :: gxyz(lx1,ly1,lz1,2*ldim,lelt)
+
+      real, intent(in) :: dxm1(lx1,lx1)
+      real, intent(in) :: dxtm1(lx1,lx1)
+
+      real rtmp,stmp,ttmp,wijke
+      real G00, G01, G02
+     $     G10, G11, G12
+     $     G20, G21, G22
+      real, shared :: shdxm1(lx1,ly1)
+      real, shared :: shus(lx1,ly1)
+      real, shared :: shur(lx1,ly1)
+      real, shared :: shu(lx1,ly1)
+      real t_kk(lz1)
+      real rut
+      real r_u(lz1)
+      real r_w(lz1)
+      integer l,e,i,j,k,kk,n
+
+      e = blockIdx%x
+      k = threadIdx%z
+      j = threadIdx%y
+      i = threadIdx%x
+      do kk = 1, lz1
+         r_u(kk) = u(i,j,kk,e)
+         r_w(kk) = 0. 
+      enddo
+      shdxm1(i,j) = dxm1(i,j)
+      call syncthreads()
+
+      do kk = 1,lz1
+          G00 = gxyz(i,j,kk,1,e)
+          G01 = gxyz(i,j,kk,2,e)
+          G02 =  gxyz(i,j,kk,3,e) 
+          G11 = gxyz(i,j,kk,4,e)
+          G12 =gxyz(i,j,kk,5,e)
+          G22 = gxyz(i,j,kk,6,e)
+          ttmp = 0.0
+          shu(i,j) = r_u(kk)
+          do l = 1, lx1
+            ttmp = ttmp + shdxm1(kk,l) * r_u(l)
+          enddo
+          rtmp = 0.0
+          stmp = 0.0
+          call syncthreads()
+          do l = 1, lx1
+            rtmp = rtmp + shdxm1(i,l)  * shu(l,j)
+            stmp = stmp + shdxm1(j,l)  * shu(i,l)
+          enddo
+          shur(i,j) = G00*rtmp
+     $                 +G01*stmp
+     $                 + G02*ttmp
+          shus(i,j) = G01*rtmp
+     $                 + G11*stmp
+     $                 + G12*ttmp
+          rut =     G02*rtmp
+     $                 +G12*stmp
+     $                + G22*ttmp
+          wijke = 0.0
+          call syncthreads()
+          do l = 1, lx1
+             r_w(l)= r_w(l) + shdxm1(kk,l)*rut
+             wijke = wijke + shdxm1(l,i)  * shur(l,j) 
+     $                    + shdxm1(l,j)  * shus(i,l)
+          enddo
+          r_w(kk)  = r_w(kk)  + wijke
+      enddo
+      
+      do kk = 1,lz1
+         w(i,j,kk,e) = r_w(kk)
+      enddo
       return
       end
 
@@ -102,3 +232,4 @@ c       call syncthreads()
       end
 
 #endif
+
