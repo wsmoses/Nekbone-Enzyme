a96abfac4d9f4b279afa4ca18054d88e27cf7c2e
Martin Karp <martinkarp@localhost.localdomain>
2020-03-10 10:23:08 +0100

    fixed constant __restrict__

--- src/ax_cuda2.cu
+++ src/ax_cuda2.cu
@@ -179,7 +179,7 @@ __global__ void ax_cuda2_kernel_2d(double *w, double *u, double *gxyz, double *d
         w[ijk+e*LX1*LY1*LZ1] = wijke;
       }
 }
-__global__ void ax_cuda2_kernel(double *w, double *u, double *gxyz, double *dxm1, double *dxtm1){
+__global__ void ax_cuda2_kernel(double* __restrict__ w, const double* __restrict__ u, const double* __restrict__ gxyz, const double* __restrict__ dxm1, const double* __restrict__ dxtm1){
 /*      real, intent(out) :: w(lx1,ly1,lz1,lelt)
       real u(lx1,ly1,lz1,lelt)
       real ur  (lx1,ly1,lz1,lelt)
@@ -191,9 +191,8 @@ __global__ void ax_cuda2_kernel(double *w, double *u, double *gxyz, double *dxm1
       real, intent(in) :: dxm1(lx1,lx1)
       real, intent(in) :: dxtm1(lx1,lx1)*/
 
-  double rtmp,stmp,ttmp,wijke;
+      double rtmp,stmp,ttmp,wijke;
       __shared__ double shdxm1[LX1*LY1];
-      __shared__ double shdtxm1[LX1*LY1];
       __shared__ double shu[LX1*LY1];
       __shared__ double shur[LX1*LY1];
       __shared__ double shus[LX1*LY1];
@@ -210,7 +209,7 @@ __global__ void ax_cuda2_kernel(double *w, double *u, double *gxyz, double *dxm1
       ele = e*LX1*LY1*LZ1;
 
       shdxm1[ij] = dxm1[ij];
-      shdtxm1[ij] = dxtm1[ij];
+      #pragma unroll
       for( k = 0; k < LZ1; ++k){
         ru[k] = u[ij + k*LX1*LY1 + ele];
         rw[k] = 0.0;
@@ -228,11 +227,11 @@ __global__ void ax_cuda2_kernel(double *w, double *u, double *gxyz, double *dxm1
         G11 = gxyz[ijk+3*LX1*LY1*LZ1+ele*6];
         G12 = gxyz[ijk+4*LX1*LY1*LZ1+ele*6];
         G22 = gxyz[ijk+5*LX1*LY1*LZ1+ele*6];
- 
+        __syncthreads(); 
         ttmp = 0.0;
         shu[ij] = ru[k];
         for (l = 0; l<LX1; l++){
-          ttmp += shdtxm1[l + k*LX1] * ru[l];
+          ttmp += shdxm1[k+l*LX1] * ru[l];
         }
         __syncthreads();
  
@@ -246,21 +245,21 @@ __global__ void ax_cuda2_kernel(double *w, double *u, double *gxyz, double *dxm1
         shur[ij] = G00*rtmp
                  + G01*stmp
                  + G02*ttmp;
-        shus[ij] = G01*rtmp
-                 + G11*stmp
-                 + G12*ttmp;
         rut      = G02*rtmp
                  + G12*stmp 
                  + G22*ttmp;
+        shus[ij] = G01*rtmp
+                 + G11*stmp
+                 + G12*ttmp;
 
       __syncthreads();
 
         wijke = 0.0;
         #pragma unroll
         for (l = 0; l<LX1; l++){
-          wijke += shdxm1[l + i*LX1] * shur[l+j*LX1]
-                        + shdxm1[l + j*LX1] * shus[i+l*LX1];
-          rw[l] += shdtxm1[l+k*LX1] * rut; 
+          wijke += shdxm1[l + i*LX1] * shur[l+j*LX1];
+          rw[l] += shdxm1[k+l*LX1] * rut; 
+          wijke += shdxm1[l + j*LX1] * shus[i+l*LX1];
         }
         rw[k] += wijke;
       }
@@ -271,7 +270,7 @@ __global__ void ax_cuda2_kernel(double *w, double *u, double *gxyz, double *dxm1
 }
 
 extern "C" {
-  void ax_cuda2_(double *w, double *u, double *gxyz, double *dxm1, double *dxtm1,  int *nel){
+  void ax_cuda2_(double* __restrict__ w, const double* __restrict__ u, const double* __restrict__ gxyz, const double* __restrict__ dxm1, const double* __restrict__ dxtm1, const int *nel){
     ax_cuda2_kernel<<<*nel,dim3(LX1,LY1,1)>>>(w, u, gxyz, dxm1, dxtm1);
 }
 }
